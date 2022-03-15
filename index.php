6e1b5e6abe244f7f7ec04a8ac0ac2a39cda191d0
Kaveeshafdo <kaveesha.shavinda1999@gmail.com>
2022-03-15 14:56:45 +0530

    edit file

--- index.php
+++ index.php
@@ -5,30 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 
 <body>
-
-<div class="header">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <div class="container">
-            <a class="navbar-brand" style="color: #669999; font-weight: 900; font-size: 30px" href="index.php">Visitors</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="index.php">Home</a>
-                    </li>                
-                </ul>
-            </div>
-        </div>
-    </nav>
-</div>
 
     <div class="body">
         <div class="post-form">
@@ -48,10 +29,15 @@
 </html>
 
 <?php
-//require_once 'FileHandle.php';
+require_once 'FileHandle.php';
 
-//$fileHandle = new FileHandle();
-//$fileHandle->readFile();
+$fileHandle = new FileHandle();
+//$fileHandle->writeFile($publisher,$postContent,$trn_date);
+$final = $fileHandle->readFile();
+foreach($final as $line){
+   echo $line[0].$line[1].$line[2]."\n";
+
+}
 
 
 ?>
