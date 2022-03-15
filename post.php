fedd4291cd27f7554bfa2af589f6d0c33896ab00
Kaveeshafdo <kaveesha.shavinda1999@gmail.com>
2022-03-15 12:07:32 +0530

    create csv file and read function

--- post.php
+++ post.php
@@ -3,7 +3,11 @@
    // echo date_default_timezone_get().'<br />';
       $trn_date = date("Y-m-d H:i:s");
     
-    
+      $file = 'C:\\xampp\\htdocs\\Visitors\\info.csv';
+   $fh = fopen($file, 'r');
+   while(list($name,$post,$datetime) = fgetcsv($fh,1024,',')){
+              printf("<p>%s , %s , %s</p>",$name,$post,$datetime);
+   }
 
 
 ?>
