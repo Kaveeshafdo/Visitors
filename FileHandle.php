d6cbb2e96c4a90d6a9dcdb001688d286f6928a52
sheronfdo <jamithsheron5@gmail.com>
2022-03-15 14:14:17 +0530

    create array for post content

--- FileHandle.php
+++ FileHandle.php
@@ -4,12 +4,13 @@ class FileHandle
 {
 
     public $file = 'info.csv';
+    public $arrContent;
 
     public function readFile()
-    {
+    {   
         $fh = fopen($this->file, 'r');
         while (list($name, $post, $datetime) = fgetcsv($fh, 1024, ',')) {
-            printf("<p>%s , %s , %s</p>", $name, $post, $datetime);
+            $this->arrContent = array_merge($this->arrContent, array($name,$post,$datetime));
         }
     }
 
